51753c20403bafbcee1c1c1afb9d26a0bda1394c
Bryamjim <93013122+Bryamjim@users.noreply.github.com>
2022-06-06 08:00:48 -0500

    Add files via upload

---
+++ trab-final-1bim-grupo11/AdministrarInmobiliaria/src/paquete02/Propietario.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete02;
+
+/**
+ *
+ * @author reroes
+ */
+public class Propietario {
+    private String nombres;
+    private String apellidos;
+    private String identificación;
+    
+    
+}
---
+++ trab-final-1bim-grupo11/AdministrarInmobiliaria/src/paquete06/Casa.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete06;
+
+/**
+ *
+ * @author reroes
+ */
+public class Casa {
+    private String nombre_constructora;
+    private String id_de_la_empresa;
+    
+
+    private String nombre_ciudad;
+    private String nombre_provincia;
+
+
+    private String nombre_barrio;
+    private String referencia;
+    
+
+    private int numero_de_cuartos;
+    private double número_de_metros_cuadrados;
+    private double precio_por_metro_cuadrado;
+    private double costo_final;
+    
+    
+}
